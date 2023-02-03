f1aff6e86a672180e1dace24fb5041b07ad306f0
Esvenn <L_esvenn@yahoo.com>
2023-02-03 10:27:37 +0000

    Second commit

--- src/main/java/com/mycompany/craps/game/CrapsGame.java
+++ src/main/java/com/mycompany/craps/game/CrapsGame.java
@@ -12,6 +12,6 @@ package com.mycompany.craps.game;
 public class CrapsGame {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome");
     }
 }
